f650f908008060a3f1c502d2b46224945b225ed4
Ivan Janjić <wannjanjic@gmail.com>
2023-07-19 08:14:20 +0200

    add custom components and assets

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topCenter;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key, required this.from, required this.to});
+  const GradientContainer.purple({super.key, 
+    this.from = Colors.deepPurple,
+    this.to = Colors.indigo});
+
+  final Color from;
+  final Color to;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [from, to],
+            begin: startAlignment,
+            end: endAlignment,
+          ),
+        ),
+        child: Center(
+          child: Image.asset('assets/images/dice-2.png'),
+        ),
+      );
+  }
+}
--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,25 +1,12 @@
+import 'package:first_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Color.fromARGB(255, 41, 16, 85), Color.fromARGB(255, 87, 63, 154)],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        child: const Center(
-          child: Text(
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-              ),
-              "Hello world!"),
-        ),
-      ),
+      body: GradientContainer(
+        from: Color.fromARGB(255, 28, 5, 92),
+        to: Color.fromARGB(255, 67, 50, 97))
     ),
   ));
 }
