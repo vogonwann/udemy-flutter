107d16d3d03ac58a43f1b2ae69eea14f4a761d25
Ivan Janjić <wannjanjic@gmail.com>
2023-07-24 18:35:56 +0200

    add advanced basci assignemnet

---
+++ adv_basics/lib/results_screen.dart
@@ -0,0 +1,68 @@
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/question_summary.dart';
+import 'package:flutter/material.dart';
+
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen(
+      {super.key, required this.chosenAnswers, required this.startOver});
+
+  final List<String> chosenAnswers;
+  final void Function() startOver;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].text,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenAnswers[i]
+        },
+      );
+    }
+
+    return summary;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((item) {
+      return item['user_answer'] == item['correct_answer'];
+    }).length;
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly!',
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            QuestionSummary(summaryData),
+            const SizedBox(
+              height: 30,
+            ),
+            TextButton.icon(
+              icon: const Icon(Icons.refresh),
+              style: TextButton.styleFrom(
+                iconColor: Colors.white,
+                foregroundColor: Colors.white,
+              ),
+              onPressed: startOver,
+              label: const Text('Restart quiz!'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
